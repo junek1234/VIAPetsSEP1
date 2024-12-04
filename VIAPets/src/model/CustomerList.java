5d0fc6599c057da483e5905d85ffa77fdc037200
Guillermo Sánchez Martínez <guigames2126@gmail.com>
2024-12-04 12:12:42 +0100

    nothing

--- VIAPets/src/model/CustomerList.java
+++ VIAPets/src/model/CustomerList.java
@@ -22,9 +22,7 @@ public class CustomerList {
         customer.setName(newName);
         customer.setPhoneNumber(newPhoneNumber);
         customer.setEmail(newEmail);
-      }
-
-
+    }
 
   // Remove a customer by ID
   public void removeCustomer(Customer customer) {
