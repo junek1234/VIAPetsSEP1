91095c362ca9140abc9e46aabaee6700f2551356
Piotr Junosz <piotrju2005@wp.pl>
2024-12-08 17:42:54 +0100

    we are going to eat soon

--- VIAPets/src/view/ViewHandlerTest.java
+++ VIAPets/src/view/ViewHandlerTest.java
@@ -1,11 +1,11 @@
-package view;
-
-import javafx.application.Application;
-
-public class ViewHandlerTest
-{
-  public static void main(String[] args)
-  {
-    Application.launch(ViewHandler.class);
-  }
-}
+//package view;
+//
+//import javafx.application.Application;
+//
+//public class ViewHandlerTest
+//{
+//  public static void main(String[] args)
+//  {
+//    Application.launch(ViewHandler.class);
+//  }
+//}
