94ebde70696f659cfe9fef60e174a5d4c0f3b78e
Piotr Junosz <piotrju2005@wp.pl>
2024-12-16 09:36:30 +0100

    adding a run class in main module to start the program

---
+++ VIAPets/src/main/RunVIAPetsApplication.java
@@ -0,0 +1,13 @@
+package main;
+
+import com.sun.jdi.connect.LaunchingConnector;
+import javafx.application.Application;
+import view.StartGUI;
+
+public class RunVIAPetsApplication
+{
+  public static void main(String[] args)
+  {
+    Application.launch(StartGUI.class);
+  }
+}
