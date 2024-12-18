dfe69d28937cdf008ea50777e421f75167c35e7d
Guillermo Sánchez Martínez <guigames2126@gmail.com>
2024-12-18 19:02:44 +0000

    math assignment

--- VIAPets/src/view/BookingViewController.java
+++ VIAPets/src/view/BookingViewController.java
@@ -24,124 +24,145 @@ public class BookingViewController
 
   public void saveAddBooking(ActionEvent actionEvent)
   {
-    int petID = 0;//need to add exception later
-    int customerID = 0;//need to add exception later
-    LocalDate startDateFromField;
-    Date startDate = null;
-    Date endDate = null;
-    LocalDate endDateFromField;
-    int startHour = 0;
-    int endHour = 0;
+    //Variable declaration and assignment
+    int petID = 0;// 1
+    int customerID = 0;// 1
+    LocalDate startDateFromField; // 1
+    Date startDate = null; // 1
+    Date endDate = null;// 1
+    LocalDate endDateFromField;// 1
+    int startHour = 0; // 1
+    int endHour = 0; // 1
+    //Checking if all fields are empty.
     if (bookingPetIDTextField.getText().isEmpty()
         || bookingCustomerIDTextField.getText().isEmpty()
         || bookingStartDateDatePicker.getValue() == null
-        || bookingEndDateDatePicker.getValue() == null)
+        || bookingEndDateDatePicker.getValue() == null) // 4 getters and 2 isEmpty(6) in total
     {
-      Alert alert1 = new Alert(Alert.AlertType.ERROR);
-      alert1.setTitle("Error");
-      alert1.setHeaderText(null);
-      alert1.setContentText("Invalid input!");
-      alert1.show();
+      //Create and display the alert
+      Alert alert1 = new Alert(Alert.AlertType.ERROR); // 1
+      alert1.setTitle("Error"); // 1
+      alert1.setHeaderText(null); // 1
+      alert1.setContentText("Invalid input!"); // 1
+      alert1.show(); // 1
       return;//it stops the method when catching exception
     }
     else
     {
       try
       {
-        petID = Integer.parseInt(bookingPetIDTextField.getText());
-        customerID = Integer.parseInt(bookingCustomerIDTextField.getText());
+        //Assigning values to the variables.
+        petID = Integer.parseInt(bookingPetIDTextField.getText()); // 1 parseInt and 1 getter. This takes 2
+        customerID = Integer.parseInt(bookingCustomerIDTextField.getText()); // 1 parseInt and 1 getter. This takes 2
         startDateFromField = bookingStartDateDatePicker.getValue();
         startDate = new Date(startDateFromField.getDayOfMonth(),
-            startDateFromField.getMonthValue(), startDateFromField.getYear());
-        endDateFromField = bookingEndDateDatePicker.getValue();
+            startDateFromField.getMonthValue(), startDateFromField.getYear()); // This takes 5 (assignation + getters
+        endDateFromField = bookingEndDateDatePicker.getValue(); // Getter and assignment. 2
         endDate = new Date(endDateFromField.getDayOfMonth(),
-            endDateFromField.getMonthValue(), endDateFromField.getYear());
+            endDateFromField.getMonthValue(), endDateFromField.getYear()); // This takes 5 (assignation + getters
+
+
+
 
       }
       catch (NumberFormatException e)
       {
-        Alert alert1 = new Alert(Alert.AlertType.ERROR);
-        alert1.setTitle("Error");
-        alert1.setHeaderText(null);
-        alert1.setContentText("Invalid input!");
-        alert1.show();
+        //Create and display the alert
+        Alert alert1 = new Alert(Alert.AlertType.ERROR); // 1
+        alert1.setTitle("Error");// 1
+        alert1.setHeaderText(null);// 1
+        alert1.setContentText("Invalid input!");// 1
+        alert1.show();// 1
         return;//it stops the method when catching exception
       }
 
-      MyModelManager manager = new MyModelManager();
 
-      Pet bookingPet = manager.getAllPets().getPetByID(petID);
+      MyModelManager manager = new MyModelManager(); // 1
+
+
+      Pet bookingPet = manager.getAllPets().getPetByID(petID); // Getter plus assignment. This takes 2
       Customer bookingCustomer = manager.getAllCustomers()
-          .getCustomer(customerID);
-      DateInterval dateInterval = new DateInterval(startDate,endDate);
-      //errors
+          .getCustomer(customerID); // Getter plus assignment. This takes 2
+      DateInterval dateInterval = new DateInterval(startDate,endDate); // Getter plus assignment. This takes 2
+
       if(!manager.isThisPeriodAvailable(dateInterval))
       {
-        Alert alert1 = new Alert(Alert.AlertType.ERROR);
-        alert1.setTitle("Error");
-        alert1.setHeaderText(null);
-        alert1.setContentText("This period is not available!");
-        alert1.show();
+        //Create and display the alert
+        Alert alert1 = new Alert(Alert.AlertType.ERROR); // 1
+        alert1.setTitle("Error"); // 1
+        alert1.setHeaderText(null); // 1
+        alert1.setContentText("This period is not available!"); // 1
+        alert1.show(); // 1
       }
       else if (bookingPet == null)
       {
-        Alert alert1 = new Alert(Alert.AlertType.ERROR);
-        alert1.setTitle("Error");
-        alert1.setHeaderText(null);
-        alert1.setContentText("No pet with ID: " + petID + "!");
-        alert1.show();
+        //Create and display the alert
+        Alert alert1 = new Alert(Alert.AlertType.ERROR); // 1
+        alert1.setTitle("Error"); // 1
+        alert1.setHeaderText(null); // 1
+        alert1.setContentText("No pet with ID: " + petID + "!"); // 1
+        alert1.show(); // 1
+
 
       }
       else if (bookingPet.getStatus().equals("Not Sold"))
       {
-        Alert alert1 = new Alert(Alert.AlertType.ERROR);
-        alert1.setTitle("Error");
-        alert1.setHeaderText(null);
-        alert1.setContentText("This pet has not been sold yet!");
-        alert1.show();
+        //Create and display the alert
+        Alert alert1 = new Alert(Alert.AlertType.ERROR); // 1
+        alert1.setTitle("Error"); // 1
+        alert1.setHeaderText(null); // 1
+        alert1.setContentText("This pet has not been sold yet!"); // 1
+        alert1.show(); // 1
       }
       else if (bookingCustomer == null)
       {
-        Alert alert1 = new Alert(Alert.AlertType.ERROR);
-        alert1.setTitle("Error");
-        alert1.setHeaderText(null);
-        alert1.setContentText("No customer with ID: " + customerID + "!");
-        alert1.show();
+        //Create and display the alert
+        Alert alert1 = new Alert(Alert.AlertType.ERROR); // 1
+        alert1.setTitle("Error"); // 1
+        alert1.setHeaderText(null); // 1
+        alert1.setContentText("No customer with ID: " + customerID + "!"); // 1
+        alert1.show(); // 1
       }
       else
       {
-        for (int i = 0; i < manager.getAllBookings().getBookings().size(); i++)
+        for (int i = 0; i < manager.getAllBookings().getBookings().size(); i++) // Checking if there is a slot available for all the days of the new booking. n
         {
-          if((manager.getAllBookings().getBookings().get(i).getPet().getPetID()==petID))
-              if((dateInterval.isBetween(manager.getAllBookings().getBookings().get(i).getDateInterval()
+          //Check if a pet already has a booking for that period.       if((manager.getAllBookings().getBookings().get(i).getPet().getPetID()==petID))
+          if((dateInterval.isBetween(manager.getAllBookings().getBookings().get(i).getDateInterval()
               .getStartDate()))||(dateInterval.isBetween(manager.getAllBookings().getBookings().get(i).getDateInterval()
               .getEndDate())))
           {
-            Alert alert1 = new Alert(Alert.AlertType.ERROR);
-            alert1.setTitle("Error");
-            alert1.setHeaderText(null);
-            alert1.setContentText("There is already a booking for this pet in this period!");
-            alert1.show();
+            //Create and display the alert
+            Alert alert1 = new Alert(Alert.AlertType.ERROR); //1
+            alert1.setTitle("Error"); //1
+            alert1.setHeaderText(null); //1
+            alert1.setContentText("There is already a booking for this pet in this period!"); //1
+            alert1.show(); //1
             return;
           }
         }
 
+
         //if everything is fine
+
 
         try
         {
-          bookingPet.setLocation("Kennel");
-          manager.editPet(petID, bookingPet);
+          bookingPet.setLocation("Kennel"); //Change the location of the pet to kennel. This takes 1
+          manager.editPet(petID, bookingPet); //1
           Booking newBooking = new Booking(MyModelManager.createNextBookingID(),
-              bookingPet, bookingCustomer, dateInterval);
+              bookingPet, bookingCustomer, dateInterval); // Assignment + create next booking id. This takes 1 + 13 = 14.
 
-          manager.addBooking(newBooking);
+
+          manager.addBooking(newBooking); //1
           Stage stage = (Stage) ((Button) actionEvent.getSource()).getScene()
-              .getWindow();
-          stage.close();
-          MyModelManager managerUpdateLists = new MyModelManager();
+              .getWindow(); //Assignment plus 3 getters. This takes 4
+          stage.close(); //1
+          MyModelManager managerUpdateLists = new MyModelManager(); //1
 
-          XMLHandler.updateXML();
+
+          XMLHandler.updateXML(); //1
+
 
         }
         catch (IOException e)
@@ -149,12 +170,29 @@ public class BookingViewController
           throw new RuntimeException(e);
         }
 
+
       }
+
 
     }
 
-  }
+//     We have no recursion so we do not need a base case.
+//     We loop n times in the for loop because that is the length of the bookings list.
+//     T(n) = 96 + n, so ignoring constants, we get T(n) = O(n).
+//     We chose this method because it is the one with the biggest time complexity in our code.
+//
+//
+//     Dominating Term Analysis
+//     T(n) = 96 + n
+//     - Dominating Term: n
+//     - Coefficient Analysis: The coefficient 1 comes from the operations performed in each iteration of both the while and for loops.
+//     -Explanation: The coefficient 1 influences practical runtime by multiplying the n factor.
+//
+//     Optimisation Suggestion
+//     -By creating the alerts outside the ifs and just changing the text and showing them inside  the ifs would reduce the constant time complexity by almost a half.
+//
 
+  }
 
 }
 
